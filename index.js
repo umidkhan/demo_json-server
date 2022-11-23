d28a8a40b4d902ee3b26257d4de4458ccc7f5e5f
umidkhan <umidxondeveloper@mail.ru>
2022-11-23 16:30:35 +0500

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonSever = require("json-server");
 const server = jsonSever.create();
 const router = jsonSever.router("db.json");
 const middlewares = jsonSever.defaults();
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
